20cce4fe64e546a7188587c5481b663a34f10d04
Akash_sharma <akashakash87928@gmail.com>
2024-10-17 19:41:35 +0530

    I Learn C-language

---
+++ Chapter4_Practice/07_problem7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int sum = 0;
+    for (int i = 1; i <10; i++)
+    {
+        sum +=8*i;
+    }
+        printf("Sum of the table is 8 %d\n", sum);
+    
+    return 0;
+}
